669ab100e7fbf8573521870b6f5dfce733bbd2c9
RubenisR <r.rubeniszeleznovs@student.fontys.nl>
2025-05-15 14:57:19 +0200

    Everything is set up finally

--- convert_checkpoint.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-# Load GPU checkpoint onto CPU
-state = torch.load(
-    "apps/radiology/model/pretrained_deepedit_dynunet.pt",
-    map_location=torch.device("cpu"),
-)
-# Save back as a CPU checkpoint
-torch.save(state, "apps/radiology/model/pretrained_deepedit_cpu.pt")
